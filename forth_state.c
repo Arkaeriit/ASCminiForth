23e5422a2129a2748bee99cfb62091c388b77a3d
Arkaeriit <maxbouillot@gmail.com>
2021-04-28 21:20:31 +0200

    Fixed exit function

--- forth_state.c
+++ forth_state.c
@@ -28,6 +28,7 @@ void amf_clean_state(forth_state_t* fs){
 
 //Pops the last element from the data stack
 word_t amf_pop_data(forth_state_t* fs){
+    printf("pop data at index: %zi\n",fs->data->stack_pointer-1);
     word_t ret = fs->data->stack[fs->data->stack_pointer-1];
     fs->data->stack_pointer--;
     return ret;
@@ -35,18 +36,21 @@ word_t amf_pop_data(forth_state_t* fs){
 
 //Push a new element to the data stack
 void amf_push_data(forth_state_t* fs, word_t w){
+    printf("push data at index: %zi\n",fs->data->stack_pointer);
     fs->data->stack[fs->data->stack_pointer] = w;
     fs->data->stack_pointer++;
 }
 
 //Push a code_pointer element on the code stack
 void amf_push_code(forth_state_t* fs, code_pointer_t p){
+    printf("push code at index: %zi\n",fs->code->stack_pointer);
     fs->code->stack[fs->code->stack_pointer] = p;
     fs->code->stack_pointer++;
 }
 
 //Pop a code_pointer element from the code stack
 code_pointer_t amf_pop_code(forth_state_t* fs){
+    printf("pop code at index: %zi\n",fs->code->stack_pointer-1);
     code_pointer_t ret = fs->code->stack[fs->code->stack_pointer-1];
     fs->code->stack_pointer--;
     return ret;
@@ -57,6 +61,9 @@ void amf_exit(forth_state_t* fs){
 	printf("Returning.\n");
 	code_pointer_t previous_pos = amf_pop_code(fs);
 	fs->pos = previous_pos;
+    entry_t e;
+    amf_find(fs->dic, &e, NULL, fs->pos.code.current_word);
+    fs->current_word_copy = e.func.F_word;
 }
 
 //Run a single step of user Forth code, if needed
@@ -72,13 +79,13 @@ void amf_run_step(forth_state_t* fs){
 	}
 	//Otherwize, we run the part of the curent word we are pointing to
 	word_node_t current_node = fs->current_word_copy->content[fs->pos.code.pos_in_word];
+	fs->pos.code.pos_in_word++;
 	if(current_node.type == normal_word){
-		printf("Calling hash %u.\n",current_node.content.hash);
+		printf("Calling hash %u from pos %zi.\n",current_node.content.hash, fs->pos.code.pos_in_word - 1);
 		amf_call_func(fs, current_node.content.hash);
 	}else{
 		printf("Handling special word.\n");
 		//TODO: handle special words
 	}
-	fs->pos.code.pos_in_word++;
 }
 
