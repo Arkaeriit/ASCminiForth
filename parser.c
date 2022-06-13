b1779d7b5d05cd14d9638d649cc8f4e81689d1d9
Maxime Bouillot <maxime.bouillot@withings.com>
2022-06-13 22:50:18 +0200

    Added escaped quotes and line comments.

--- parser.c
+++ parser.c
@@ -2,8 +2,8 @@
 #include "stdio.h"
 #include "string.h"
 
-parser_state_t* amf_init_parser(void){
-    parser_state_t* ret = malloc(sizeof(parser_state_t));
+parser_state_t *amf_init_parser(void) {
+    parser_state_t *ret = malloc(sizeof(parser_state_t));
     ret->fs = amf_init_state();
     ret->buffer = malloc(PARSER_BUFFER_SIZE);
     ret->custom_word_name = malloc(PARSER_CUSTOM_NAME_SIZE);
@@ -13,11 +13,13 @@ parser_state_t* amf_init_parser(void){
     ret->writing_definition_s_name = false;
     ret->is_in_parenthesis = false;
     ret->is_between_quotes = false;
+    ret->is_last_escaped = false;
+    ret->wait_for_new_line = false;
     amf_init_io();
     return ret;
 }
 
-void amf_clean_parser(parser_state_t* parse){
+void amf_clean_parser(parser_state_t * parse) {
     amf_clean_io();
     free(parse->custom_word_name);
     free(parse->buffer);
@@ -25,45 +27,59 @@ void amf_clean_parser(parser_state_t* parse){
     free(parse);
 }
 
-void amf_parse_char(parser_state_t* parse, char ch){
-    if(ch == '"'){
+void amf_parse_char(parser_state_t * parse, char ch) {
+	if (parse->wait_for_new_line) {
+		if (ch == '\n') {
+			parse->wait_for_new_line = false;
+		} else {
+			return;
+		}
+	}
+	if (ch == '\\' && !parse->is_last_escaped) {
+		parse->is_last_escaped = true;
+		return;
+	}
+    if (ch == '"' && !parse->is_last_escaped) {
         parse->is_between_quotes = !parse->is_between_quotes;
     }
-    if(ch == '('){
+    if (ch == '(') {
         parse->is_in_parenthesis = true;
-    }else if(parse->is_in_parenthesis){
-        if(ch == ')'){
+    } else if (parse->is_in_parenthesis) {
+        if (ch == ')') {
             parse->is_in_parenthesis = false;
         }
-    }else if(ch == ':' && !parse->is_between_quotes){
-        if(!parse->in_def){
+    } else if (ch == ':' && !parse->is_between_quotes) {
+        if (!parse->in_def) {
             parse->in_def = true;
             parse->writing_definition_s_name = true;
-        }else{
+        } else {
             //error
         }
-    }else if(ch == ';' && !parse->is_between_quotes){
-        if(parse->in_def){
+    } else if (ch == ';' && !parse->is_between_quotes) {
+        if (parse->in_def) {
             parse->in_def = false;
             parse->buffer[parse->pnt] = 0;
             parse->in_word = false;
             parse->pnt = 0;
             amf_compile_string(parse->fs->dic, parse->custom_word_name, parse->buffer);
-        }else{
+        } else {
             //error
         }
-    }else if(amf_is_delimiter(ch) && !parse->is_between_quotes){
-        if(parse->in_word){
-            if(parse->writing_definition_s_name){
+    } else if (amf_is_delimiter(ch) && !parse->is_between_quotes) {
+		if (parse->is_last_escaped) {
+			parse->wait_for_new_line = true;
+		}
+        if (parse->in_word) {
+            if (parse->writing_definition_s_name) {
                 parse->buffer[parse->pnt] = 0;
                 parse->in_word = false;
                 parse->pnt = 0;
                 parse->writing_definition_s_name = false;
                 strcpy(parse->custom_word_name, parse->buffer);
-            }else if(parse->in_def){
+            } else if (parse->in_def) {
                 parse->buffer[parse->pnt] = ch;
                 parse->pnt++;
-            }else{
+            } else {
                 parse->buffer[parse->pnt] = 0;
                 parse->in_word = false;
                 parse->pnt = 0;
@@ -71,34 +87,35 @@ void amf_parse_char(parser_state_t* parse, char ch){
                 amf_executes_node(parse->fs, &node_to_exe);
                 amf_run(parse->fs);
             }
-        }else if(parse->in_def && !parse->writing_definition_s_name){
+        } else if (parse->in_def && !parse->writing_definition_s_name) {
             parse->buffer[parse->pnt] = ch;
             parse->pnt++;
         }
-    }else{
+    } else {
         parse->in_word = true;
         parse->buffer[parse->pnt] = ch;
         parse->pnt++;
-    }        
+    }
+	parse->is_last_escaped = false;
 }
 
-void amf_shell(void){
+void amf_shell(void) {
     amf_print_string("Starting the ASCminiForth shell.\n");
-    parser_state_t* parse = amf_init_parser();
-    while(parse->fs->running){
+    parser_state_t *parse = amf_init_parser();
+    while (parse->fs->running) {
         amf_parse_char(parse, amf_input());
     }
     amf_clean_parser(parse);
 }
 
 #if AMF_USE_SOURCE_FILE
-error amf_register_file(parser_state_t* p, const char* filemane){
-    FILE* f = fopen(filemane, "r");
-    if(f == NULL){
+error amf_register_file(parser_state_t * p, const char *filemane) {
+    FILE *f = fopen(filemane, "r");
+    if (f == NULL) {
         return invalid_file;
     }
     int ch = fgetc(f);
-    while(ch != EOF){
+    while (ch != EOF) {
         amf_parse_char(p, ch);
         ch = fgetc(f);
     }
@@ -106,4 +123,3 @@ error amf_register_file(parser_state_t* p, const char* filemane){
     return OK;
 }
 #endif
-
--- parser.h
+++ parser.h
@@ -4,14 +4,16 @@
 
 typedef struct {
     forth_state_t* fs;
-    char* buffer; //Store the characters being currentely processed
-    char* custom_word_name; //Stores a copy of the name of the word being defined
-    int pnt; //Pointer to the curent portiotion of buffer where we are writing
-    bool in_word; //Are we writing a word or whitespace
-    bool in_def;  //Are we writing a definition
-    bool writing_definition_s_name; //Are we writing the first word of a definition (it's name)
-    bool is_in_parenthesis; //are we in a ( xx ) comment
-    bool is_between_quotes; //Are we betweens " quotes "
+    char* buffer;                   // Store the characters being currentely processed
+    char* custom_word_name;         // Stores a copy of the name of the word being defined
+    int pnt;                        // Pointer to the curent portiotion of buffer where we are writing
+    bool in_word;                   // Are we writing a word or whitespace
+    bool in_def;                    // Are we writing a definition
+    bool writing_definition_s_name; // Are we writing the first word of a definition (it's name)
+    bool is_in_parenthesis;         // Are we in a ( xx ) comment
+    bool is_between_quotes;         // Are we betweens " quotes "
+	bool is_last_escaped;           // Is the previous character '\'
+	bool wait_for_new_line;         // Not parsing until new line because we are in a comment
 } parser_state_t;
 
 parser_state_t* amf_init_parser(void);
